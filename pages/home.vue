6fe914cbafab9c7fec0f94a0b15402e96a64e76f
Pavana sri vinaya <pavanasri.nyros@gmail.com>
2020-12-14 21:49:41 +0530

    first

---
+++ pages/home.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container>
+    <div id="my_div" class="fatsecret_container"></div>
+  </v-container>
+</template>
---
+++ pages/inspire.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+     <v-container>
+    <div id="my_div" class="fatsecret_container"></div>
+  </v-container>
+    </v-col>
+  </v-row>
+</template>
